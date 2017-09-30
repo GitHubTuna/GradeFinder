a8aa13a8fc653820573be9f5d70e9b75fe44fa9d
Fortuna Vaio <31992693+GitHubTuna@users.noreply.github.com>
2017-09-30 13:45:30 -0400

    Added Code to Project

---
+++ GradeFinder/Assessment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeFinder
+{
+    interface Assessment
+    {
+        string Description { get; set; }
+        double Score { get; set; }
+    }
+}
--- GradeFinder/Program.cs
+++ GradeFinder/Program.cs
@@ -10,7 +10,56 @@ namespace GradeFinder
     {
         static void Main(string[] args)
         {
-            //GradeFinder
+            //Add code to ask what assessment types the student has
+            //Prompt only for Assessments that exist
+
+            Console.WriteLine("How many exams did you have before your final");
+            int numExams=int.Parse(Console.ReadLine());
+
+            if (numExams > 0)
+            {
+                Console.WriteLine("What was the weight of your final grade given to these exams?");
+                Console.WriteLine("enter 20% as 20)");
+                int weight = int.Parse(Console.ReadLine());
+
+                var exams = new List<Exam>();
+
+                for (int i = 0; i < numExams; i++)
+                {
+                    exams.Add(new Exam { Description = ("Test " + i).ToString(), Score = -999 });
+                }
+
+                List<int> examScores = new List<int>();
+
+                Console.WriteLine("What were your scores on these exams? ");
+                Console.WriteLine("enter as 80, 90, 85, 70)");
+                char[] Delimiters = new char[] { ',' };
+                string[] Input = Console.ReadLine().Split(Delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < Input.Length; i++)
+                {
+                    exams[i].Score = int.Parse(Input[i]);
+                }
+
+                int untakenExams = exams.Where(o => o.Score == -999).Count();
+
+                if (untakenExams > 0)
+                {
+                    //Received null error on this update when all exams had a score
+                    exams.Where(o => o.Score == -999).FirstOrDefault().Score = 0;
+                }
+                for (int i = 0; i < exams.Count; i++)
+                {
+                    Console.WriteLine(exams[i].Description + " " + exams[i].Score);
+                }
+
+                Console.WriteLine("You have " + untakenExams + " untaken exams, correct?");
+                Console.ReadLine();
+                //Code continues...
+            
+
+            }
+
         }
     }
 }
